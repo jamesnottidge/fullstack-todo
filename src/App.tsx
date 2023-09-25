7969e08abbae22abcb72318360e6155310ae9c00
jamesnottidge <freshice57@gmail.com>
2023-09-25 16:51:17 +0100

    server response setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<string>("james");
+
+  const API = axios.create({
+    baseURL: "http://localhost:3002",
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  useEffect(() => {
+    API.get("/", {})
+      .then((res) => {
+        console.log(res.data);
+        setCount(res.data);
+      })
+      .catch((err) => console.log("me", err));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Hello {count} </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
